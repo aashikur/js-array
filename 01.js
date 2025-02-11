0ac7a58b63a42e5b49696c253b2d1a1db24729c3
aashikur <mdaashikur@gmail.com>
2025-02-11 19:33:45 +0600

    02.js

---
+++ 01.js
@@ -0,0 +1,21 @@
+/*
+    Concept of Array
+    store various value/data of same data type in single variable;
+
+
+*/
+
+// declare array
+const nums = [22,34,45,65]; // array
+const names = ['ashik', 'ovi', 'hridoy', 'bulbul'];
+const passed = [true, true, false, true , false];
+const mixedArray = [12, 'ashik', true, 4.55];
+
+// output length 
+console.log("the length is: " + passed.length);
+
+// Access Array Element - Index         
+console.log("First Element : "+ names[0]);
+
+// Accessing all array :) 
+console.log(names);
---
+++ 02.js
@@ -0,0 +1,6 @@
+nums = [1,2,4,6,8];
+console.log(nums);
+
+
+nums[1] = 10;
+console.log(nums);
